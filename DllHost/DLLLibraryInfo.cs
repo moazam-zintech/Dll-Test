aa3a8c48b427eeb60e870651dcfb886963bac081
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-05-21 15:30:28 +0500

    DLL Library is added als here but you have to create another project and build.

---
+++ DllHost/DLLLibraryInfo.cs
@@ -0,0 +1,28 @@
+﻿/*here below code for DLL Library 
+ * Just Create New Project as Class Libary(.NetFrameWork) in c#
+ * Paste Belllo Code in th class
+ * Build the program
+ * Copy Path of DLL file and paste any ou want copy new path 
+ * Create Another Console Program and USe above Path inside the class
+ * Done
+ */
+
+/*
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DllTestLibrary
+{
+    public class HelloWorld
+    {
+        public string SayHello(string name)
+        {
+            return "Hello Good Boy "+name;
+        }
+    }
+}
+*/
