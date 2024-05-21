1a92f3ef5b022ab851e3fb33202818c74d2df691
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-05-21 15:22:12 +0500

    Add project files.

---
+++ DllHost/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Reflection;
+string dllFile = @"C:\\Users\\Muhammad.Moazam\\OneDrive\\Desktop\\DllTestLibrary.dll";
+var assembly = Assembly.LoadFile(dllFile);
+var type = assembly.GetType("DllTestLibrary.HelloWorld");
+var obj = Activator.CreateInstance(type);
+var method = type.GetMethod("SayHello");
+var result = method.Invoke(obj, new object[] { "Ali" });
+Console.WriteLine(result);
