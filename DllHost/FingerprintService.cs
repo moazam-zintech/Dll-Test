b22f7b8a7751e42010edf265906c1c9c2fdfe1ac
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-06-10 18:13:54 +0500

    FingerprintServise class added.

---
+++ DllHost/FingerprintService.cs
@@ -0,0 +1,25 @@
+﻿using libzkfpcsharp;
+
+namespace TestCodeCompiler
+{
+    public class FingerprintService
+    {
+        public FingerprintService()
+        {
+            int ret = zkfp2.Init();
+            if (ret == zkfp.ZKFP_ERR_OK)
+            {
+                Console.WriteLine("SDK initialized successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"SDK initialization failed with error code: {ret}");
+            }
+        }
+        ~FingerprintService()
+        {
+            zkfp2.Terminate();
+        }
+    }
+}
+
--- DllHost/Program.cs
+++ DllHost/Program.cs
@@ -1,9 +1,17 @@
-﻿
-using System.Reflection;
-string dllFile = @"C:\\Users\\Muhammad.Moazam\\OneDrive\\Desktop\\DllTestLibrary.dll";
-var assembly = Assembly.LoadFile(dllFile);
-var type = assembly.GetType("DllTestLibrary.HelloWorld");
-var obj = Activator.CreateInstance(type);
+﻿using System.Reflection;
+namespace DllHost
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string dllFile = @"C:\Users\Muhammad.Moazam\OneDrive\Desktop\Attendence-API\libzkfpcsharp.dll";
+            var assembly = Assembly.LoadFile(dllFile);
+            var type = assembly.GetType("libzkfpcsharp.zkfp2");
+            var obj = Activator.CreateInstance(type);
             var method = type.GetMethod("Init");
             var result = method.Invoke(obj, new object[] {});
-Console.WriteLine(result);
+            Console.WriteLine(result);
+        }
+    }
+}
