89ab4e629bf52a6f260cfa526c537af06f407034
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-06-10 18:12:51 +0500

    Another Method FingerprintServise added to load ZKFingerSDK is added.

--- DllHost/Program.cs
+++ DllHost/Program.cs
@@ -4,6 +4,6 @@ string dllFile = @"C:\\Users\\Muhammad.Moazam\\OneDrive\\Desktop\\DllTestLibrary
 var assembly = Assembly.LoadFile(dllFile);
 var type = assembly.GetType("DllTestLibrary.HelloWorld");
 var obj = Activator.CreateInstance(type);
-var method = type.GetMethod("SayHello");
-var result = method.Invoke(obj, new object[] { "Ali" });
+            var method = type.GetMethod("Init");
+            var result = method.Invoke(obj, new object[] {});
 Console.WriteLine(result);
